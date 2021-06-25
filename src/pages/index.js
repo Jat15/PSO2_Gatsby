30e8f49e3a1e49c5c68b5d7fe71f705b60d5abb7
Jat15 <jinan15@hotmail.com>
2021-06-25 23:39:46 +0200

    Modification de l'index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,21 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import "./index.sass"
+
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
+      src= "https://upload.wikimedia.org/wikipedia/fr/c/cd/Phantasy_Star_Online_Logo.png"
+      alt= "Logo de phantasy star online"
+      className= "ArticlesBanner"
     />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+  <p>Phantasy Star Online (ファンタシースターオンライン) est un jeu de rôle en ligne massivement multijoueur sorti sur Dreamcast en l'an 2000 au Japon et début 2001 en Europe et aux États-Unis. Il est souvent considéré comme le premier vrai jeu du genre sur console. Après de nombreux jeux sortis dans cette catégorie au cours des années suivantes, les joueurs le catégorisent plutôt comme un hack and slash.  </p>
+
+  <p>Un deuxième épisode, Phantasy Star Online ver.2 sortira par la suite sur la même machine. La Dreamcast arrivant alors en fin de vie, le jeu sera porté sur de nombreuses autres consoles et verra naître d'autres suites.</p>
+
   </Layout>
 )
 
