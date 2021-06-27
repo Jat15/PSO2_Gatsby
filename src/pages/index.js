bbc0d7078df32ff2f4a041d5e6a804911e48deb8
Jat15 <jinan15@hotmail.com>
2021-06-27 13:40:42 +0200

    Modification index mise en plage d'une page de news

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,65 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import "./index.sass"
 
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <StaticImage
-      src= "https://upload.wikimedia.org/wikipedia/fr/c/cd/Phantasy_Star_Online_Logo.png"
-      alt= "Logo de phantasy star online"
-      className= "ArticlesBanner"
-    />
-  <p>Phantasy Star Online (ファンタシースターオンライン) est un jeu de rôle en ligne massivement multijoueur sorti sur Dreamcast en l'an 2000 au Japon et début 2001 en Europe et aux États-Unis. Il est souvent considéré comme le premier vrai jeu du genre sur console. Après de nombreux jeux sortis dans cette catégorie au cours des années suivantes, les joueurs le catégorisent plutôt comme un hack and slash.  </p>
+const IndexPage = ({data}) => {
+    
 
-  <p>Un deuxième épisode, Phantasy Star Online ver.2 sortira par la suite sur la même machine. La Dreamcast arrivant alors en fin de vie, le jeu sera porté sur de nombreuses autres consoles et verra naître d'autres suites.</p>
+    const listArticle =data.allMarkdownRemark.nodes.map( objet => {
+        const image = getImage(objet.frontmatter.img)
 
-  </Layout>
-)
+        return <div className="cardArticle">
+            <h1>{objet.frontmatter.titre}</h1>
+            <GatsbyImage image={image} alt={`Banniére de l'articles ${objet.frontmatter.titre}` } />
+            <div>
+
+                <div
+                    dangerouslySetInnerHTML={{ __html: objet.html }}
+                />
+            </div>
+        </div>
+    })
+
+    return (
+        <Layout>
+            <Seo title="Home" />
+            <div className="containerArticles">
+                {listArticle}
+            </div>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query{
+        allMarkdownRemark(
+            filter: {frontmatter: {type: {eq: "Article"}}}
+            sort: { fields: [frontmatter___date], order: DESC} 
+        ) {
+            nodes {
+                html
+                frontmatter {
+                    titre
+                    type
+                    img{
+                        childImageSharp {
+                            gatsbyImageData(
+                                placeholder: BLURRED
+                                width: 384
+                                height: 216
+                                formats: [AUTO, WEBP, AVIF]
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
 
 export default IndexPage
