2c76883dc52bbe1a6694a65ee5e78a02042e8416
Jat15 <jinan15@hotmail.com>
2021-07-09 11:36:05 +0200

    Modification du template normal
    - Modification par rapport aux nouveaux query

--- src/templates/normal.js
+++ src/templates/normal.js
@@ -5,47 +5,47 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import "./normal.sass"
-/*
-const Pages = ({data}) =>{
-    const image = getImage(data.markdownRemark.frontmatter.img) 
 
+const Pages = ({data}) =>{
+    const image = getImage(data.subTab.markdownPage.frontmatter.img) 
+    
     return <Layout>
-            <Seo title={`${data.markdownRemark.frontmatter.title} de ${data.markdownRemark.frontmatter.game} `} />
-            <h1>{data.markdownRemark.frontmatter.title}</h1>
+            <Seo title={`${data.subTab.markdownPage.frontmatter.title} de ${data.subTab.markdownPage.frontmatter.route[0]} `} />
+            <h1>{data.subTab.markdownPage.frontmatter.title}</h1>
             {
                 image?
-                    <GatsbyImage image={image} className="borderCian" alt={`Bannière du jeu ${data.markdownRemark.frontmatter.title}` } />
+                    <GatsbyImage image={image} className="borderCian" alt={`Bannière du jeu ${data.subTab.markdownPage.frontmatter.title}` } />
                     :""
             }
             <div>
                 <div
-                    dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
+                    dangerouslySetInnerHTML={{ __html: data.subTab.markdownPage.html }}
                 />
             </div>
         </Layout>
+
 }
 
 
 export const query = graphql`
     query ($slug: String) {
-        markdownRemark(fields: {slug: {eq: $slug}}) {
-            html
-            frontmatter {
-                title
-                game
-                img {
-                    childImageSharp {
-                    gatsbyImageData(
-                        placeholder: BLURRED,
-                        formats: [AUTO, WEBP, AVIF],
-                        width: 720
-                        )
+        subTab(slug: {eq: $slug}) {
+            id
+            markdownPage {
+                html
+                frontmatter {
+                    route
+                    title
+                    img {
+                        childImageSharp {
+                            gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF], width: 720)
+                        }
                     }
                 }
             }
         }
     }
+
 `
 
 export default Pages
-*/
