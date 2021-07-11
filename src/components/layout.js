ec3cc4be3c148d43191f73ae973674b48991033e
Jat15 <jinan15@hotmail.com>
2021-07-11 17:32:14 +0200

    Modification layout code et feuille de style
    - Séparation du mini menu de racourcis
    - Suppression des import inutile
    - Suppression des styles inutile

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,13 +8,13 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import { Link } from "gatsby"
-import { StaticImage, getImage } from "gatsby-plugin-image"
+import { getImage } from "gatsby-plugin-image"
 import { BgImage } from 'gbimage-bridge';
 
 import Header from "./header"
 import Menu from "./menu"
 import Footer from "./footer"
+import ShorcutBar from "./shortcutbar"
 
 import "./layout.sass"
 
@@ -51,38 +51,29 @@ const Layout = ({ children }) => {
   const pluginImage = getImage(data.background)
 
   return (
-    <BgImage image={pluginImage}  className="imageBackground" style={style}>
-
-
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+    <BgImage
+      image={pluginImage}
+      className="imageBackground"
+      style={style}
+    >
+      <Header
+        siteTitle={data.site.siteMetadata?.title || `Title`}
+      />
       <div
         className="container" 
       >
-        <input type="checkbox" id="menuCheckbox" className="displayNone"/>
-        <aside><Menu /></aside>
-        <div className="shortcutBar">
-          <label htmlFor="menuCheckbox" className="logoMenu ">
-            <StaticImage
-              src={"../images/icon_menu.png"}
-              placeholder="blurred"
-              formats={["AUTO", "WEBP", "AVIF"]}
-              alt="Logo menu"
-              width={32}
-              layout="fixed"
-            />
-          </label>
-          <a href="https://discord.com/invite/48H8Fq8" className= "logoDiscord" target="_blank" rel="noreferrer">
-            <StaticImage
-                src="../images/discord.png"
-                placeholder="blurred" 
-                formats={["AUTO", "WEBP"]}
-                alt="Logo discord"
-                width={32}
-                layout="fixed"  
-            />
-          </a>
-        </div>
-        <main className="windowLayout">
+        <input
+          type="checkbox"
+          id="menuCheckbox"
+          className="displayNone"
+        />
+        <aside>
+          <Menu />
+        </aside>
+        <ShorcutBar />
+        <main 
+          className="windowLayout"
+        >
             <section>
               {children}
             </section>
